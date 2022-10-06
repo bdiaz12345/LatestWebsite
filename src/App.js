4b03fbe4157efcc0512d2f4936877940007d8f7a
bdiaz12345 <bdiazmusic5@gmail.com>
2022-10-06 12:17:52 -0400

    ready for action

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from "react";
 import { AppBar, Box, Toolbar, Typography, Button, IconButton, Modal } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from "react-router-dom";
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '80vw',
-    height: '90vh',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
 
 const App = () => {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const handleOpen = () => {
+        window.open(require('./assets/resume.PNG'))
+    };
     return (
         <>
             <AppBar position="static">
                 <Toolbar sx={{background: 'black', justifyContent: 'space-between'}}>
                     <Typography>Bryan Diaz</Typography>
                     <Button onClick={handleOpen} sx={{background: 'white'}}>View Resume</Button>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="modal-modal-title"
-                            aria-describedby="modal-modal-description"
-                        >
-                            <Box sx={style}>
-                                <img style={{width: '100%', height: '100%', objectFit: 'contain'}} src={require('./assets/resume.PNG')}/>
-                            </Box>
-                        </Modal>
                     {/* <IconButton
                         size="large"
                         edge="start"
@@ -46,7 +21,7 @@ const App = () => {
                 </Toolbar>
                 
             </AppBar>
-            <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '95vh', background: '#fffdfa', flexDirection: 'column'}}>
+            <Box sx={{marginTop: '5vh', display: 'flex', justifyContent: 'center', alignItems: 'center', background: '#fffdfa', flexDirection: 'column'}}>
                 <Box>
                     <img src={require('./assets/bryan.jpg')} style={{width: '20vw', height: '20vw', borderRadius: '50%'}}/>
                 </Box>
